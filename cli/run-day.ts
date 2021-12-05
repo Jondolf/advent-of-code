e3f71f36a694243a689a964ea3c253e59d3ecc20
Jondolf <joona.m.aalto@outlook.com>
2021-12-05 11:52:54 +0200

    Refactor cli

--- cli/run-day.ts
+++ cli/run-day.ts
@@ -6,8 +6,8 @@ export function runDay(year: number, day: number) {
     const { part1, part2 } = require(`../years/${year}/solutions/day-${day
       .toString()
       .padStart(2, "0")}`);
-    console.log(`\n✨ AoC ${year} day ${day} part 1:`, part1(input));
-    console.log(`✨ AoC ${year} day ${day} part 2:`, part2(input));
+    console.log(`\nDay ${day} part 1:`, part1(input));
+    console.log(`Day ${day} part 2:`, part2(input));
   } catch (error) {
     throw new Error(`Couldn't run year ${year} day ${day}.`);
   }
