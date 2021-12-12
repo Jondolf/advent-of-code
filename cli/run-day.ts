a04240cae804a29d526a36a7ff8f88ce24b2ada7
Jondolf <joona.m.aalto@outlook.com>
2021-12-12 14:18:33 +0200

    Add execution duration to solution running

--- cli/run-day.ts
+++ cli/run-day.ts
@@ -1,4 +1,5 @@
 import { getInput } from "./get-input";
+import { performance } from "perf_hooks";
 
 export function runDay(year: number, day: number) {
   try {
@@ -6,8 +7,17 @@ export function runDay(year: number, day: number) {
     const { part1, part2 } = require(`../years/${year}/solutions/day-${day
       .toString()
       .padStart(2, "0")}`);
-    console.log(`\nDay ${day} part 1:`, part1(input));
-    console.log(`Day ${day} part 2:`, part2(input));
+
+    const startTime1 = performance.now();
+    const res1 = part1(input);
+    const dur1 = performance.now() - startTime1;
+
+    const startTime2 = performance.now();
+    const res2 = part2(input);
+    const dur2 = performance.now() - startTime2;
+
+    console.log(`\nDay ${day} part 1:`, res1, `(${dur1.toFixed(2)} ms)`);
+    console.log(`Day ${day} part 2:`, res2, `(${dur2.toFixed(2)} ms)`);
   } catch (error) {
     throw new Error(`Couldn't run year ${year} day ${day}. ${error}`);
   }
