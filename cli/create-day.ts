8abc23844b84eed9cc8645a6337c71cc8a9f818e
Jondolf <joona.m.aalto@outlook.com>
2021-12-14 19:41:02 +0200

    Refactor cli and add more features
    - Puzzle descriptions now get part 2 as well, which needs session token
    - Inputs will now automatically be fetched, requires session token

--- cli/create-day.ts
+++ cli/create-day.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import axios from "axios";
 const TurndownService = require("turndown");
+const sessionCookie = fs.readFileSync("session-cookie.txt").toString();
 
 const turndownService = new TurndownService({
   headingStyle: "atx",
@@ -9,9 +10,7 @@ const turndownService = new TurndownService({
 
 export function createDay(year: number, day: number) {
   try {
-    const dir = `years/${year}/solutions/day-${day
-      .toString()
-      .padStart(2, "0")}`;
+    const dir = `years/${year}/solutions/day-${day.toString().padStart(2, "0")}`;
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
@@ -21,9 +20,7 @@ export function createDay(year: number, day: number) {
     createInputFile(dir, year, day);
     createDescriptionFile(dir, year, day);
   } catch (error) {
-    throw new Error(
-      `❌ Couldn't create year ${year} day ${day} file. ${error}`
-    );
+    throw new Error(`❌ Couldn't create year ${year} day ${day} file. ${error}`);
   }
 }
 
@@ -41,72 +38,77 @@ export function part2(input: string) {
 `;
     if (!fs.existsSync(`${dir}/index.ts`)) {
       fs.writeFileSync(`${dir}/index.ts`, daySolutionTemplate);
-      console.log(
-        `✔ Year ${year} day ${day} solution file successfully created.`
-      );
+      console.log(`✔ Year ${year} day ${day} solution file successfully created.`);
     } else {
       console.log(`Year ${year} day ${day} solution file already exists.`);
     }
   } catch (error) {
-    throw new Error(
-      `❌ Couldn't create year ${year} day ${day} solution file. ${error}`
-    );
+    throw new Error(`❌ Couldn't create year ${year} day ${day} solution file. ${error}`);
   }
 }
 
 async function createInputFile(dir: string, year: number, day: number) {
   try {
     if (!fs.existsSync(`${dir}/input.txt`)) {
-      fs.writeFileSync(`${dir}/input.txt`, "");
+      const response = await axios.get(`https://adventofcode.com/${year}/day/${day}/input`, {
+        headers: { cookie: `session=${sessionCookie}` },
+        responseType: "text",
+      });
+      const input = (response.data as string).trimEnd();
+      fs.writeFileSync(`${dir}/input.txt`, input);
       console.log(`✔ Year ${year} day ${day} input file successfully created.`);
     } else {
       console.log(`Year ${year} day ${day} input file already exists.`);
     }
   } catch (error) {
-    throw new Error(
-      `❌ Couldn't create year ${year} day ${day} input file. ${error}`
-    );
+    throw new Error(`❌ Couldn't create year ${year} day ${day} input file. ${error}`);
   }
 }
 
 async function createDescriptionFile(dir: string, year: number, day: number) {
   try {
-    if (!fs.existsSync(`${dir}/README.md`)) {
-      const description = await getPuzzleDescription(year, day);
+    const file: string | undefined = fs.readFileSync(`${dir}/README.md`).toString();
+    const description = await getDescriptionMarkdown(year, day);
+    if (!file || (file && !file.includes("Part Two") && description.includes("Part Two"))) {
       fs.writeFileSync(`${dir}/README.md`, description);
       console.log(
-        `✔ Year ${year} day ${day} description file successfully created.`
+        !file
+          ? `✔ Year ${year} day ${day} description file successfully created.`
+          : `✔ Year ${year} day ${day} part 2 description successfully created.`
       );
     } else {
       console.log(`Year ${year} day ${day} description file already exists.`);
     }
   } catch (error) {
-    throw new Error(
-      `❌ Couldn't create year ${year} day ${day} description file. ${error}`
-    );
+    throw new Error(`❌ Couldn't create year ${year} day ${day} description file. ${error}`);
   }
 }
 
-async function getPuzzleDescription(
-  year: number,
-  day: number
-): Promise<string> {
+async function getDescriptionMarkdown(year: number, day: number): Promise<string> {
   try {
-    const response = await axios.get(
-      `https://adventofcode.com/${year}/day/${day}`
+    const response = await axios.get(`https://adventofcode.com/${year}/day/${day}`, {
+      headers: { cookie: `session=${sessionCookie}` },
+      responseType: "text",
+    });
+    const markdown: string = turndownService.turndown(
+      formatDescriptionHtml(response.data, year, day)
     );
-    const html = (response.data as string)
-      .match(/<article class="day-desc">(.|\n)*?<\/article>/g)![0]
-      .replace(
-        /<h2>--- /g,
-        `<h2><a href="https://adventofcode.com/${year}/day/${day}">`
-      )
-      .replace(/ ---<\/h2>/g, "</a></h2>")
-      .replace(/<em/g, "<strong")
-      .replace(/<\/em>/g, "</strong>");
-    const markdown: string = turndownService.turndown(html);
     return markdown;
   } catch (error) {
     throw new Error(`${error}`);
   }
+}
+
+function formatDescriptionHtml(html: string, year: number, day: number): string {
+  return html
+    .match(/<article class="day-desc">(.|\n)*?<\/article>/g)!
+    .join("\n")
+    .replace(/<h2>--- /g, `<h2><a href="https://adventofcode.com/${year}/day/${day}">`)
+    .replace(
+      /<h2 id="part2">--- /g,
+      `<h2 id="part2"><a href="https://adventofcode.com/${year}/day/${day}#part2">`
+    )
+    .replace(/ ---<\/h2>/g, "</a></h2>")
+    .replace(/<em/g, "<strong")
+    .replace(/<\/em>/g, "</strong>");
 }
